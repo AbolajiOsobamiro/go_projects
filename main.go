d4a271b7fffee75c365ef1704ce5e240aa818715
AbolajiOsobamiro <144628862+AbolajiOsobamiro@users.noreply.github.com>
2024-07-19 19:49:32 +0100

    Add files via upload

--- main.go
+++ main.go
@@ -1,107 +1,130 @@
 package main
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
-	"strings"
-	"sync"
+	"io"
+	"os"
 	"time"
-)
-var conferenceName = "Go Conference"
-const conferenceTickets uint = 50
-var remainingTickets uint = 50
-var bookings = make([]userData,0)
 
-type userData struct {
-	firstName string
-	lastName string
-	email string
-	numTickets uint
+	"net/http"
+	"net/url"
+
+	"gopkg.in/yaml.v3"
+	"mvdan.cc/xurls/v2"
+
+	"golang.org/x/net/html"
+)
+
+func renderNode(n *html.Node) string {
+	var buf bytes.Buffer
+	w := io.Writer(&buf)
+	html.Render(w, n)
+	return buf.String()
 }
-var wg = sync.WaitGroup{}
+
+const hostURL string = "http://192.0.0.1"
+const filePath string = "/etc/unilag_login.yaml"
+
+type LoginData struct {
+	Username string `yaml:"username"`
+	Password string `yaml:"password"`
+}
+
+func extractLoginUrl() (string, error) {
+	client := &http.Client{
+		Timeout: 3 * time.Second,
+	}
+
+	res, err := client.Get(hostURL)
+	if err != nil {
+		return "", errors.New("Already logged in")
+	}
+	defer res.Body.Close()
+
+	doc, err := html.Parse(res.Body)
+	if err != nil {
+		return "", err
+	}
+
+	var url string
+	var crawler func(*html.Node)
+	// Crawl the html document to find the login url
+	crawler = func(node *html.Node) {
+		rxRelaxed := xurls.Relaxed()
+		// Extract the login url from the redirect link added in the meta tag
+		if node.Type == html.ElementNode && node.Data == "meta" && node.Attr[0].Val == "refresh" {
+			url = rxRelaxed.FindString(node.Attr[1].Val)
+			return
+		}
+		for child := node.FirstChild; child != nil; child = child.NextSibling {
+			crawler(child)
+		}
+	}
+
+	crawler(doc)
+	if url != "" {
+		return url, nil
+	}
+
+	return "", errors.New("Already logged in")
+}
+
+func login(hostURL string, username string, password string) error {
+	client := &http.Client{
+		Timeout: 5 * time.Second,
+	}
+
+	values := url.Values{}
+	values.Add("username", username)
+	values.Add("password", password)
+
+	res, err := client.PostForm(hostURL, values)
+	if err != nil {
+		fmt.Println(err)
+		return errors.New("Unable to login")
+	} else {
+		defer res.Body.Close()
+		return nil
+	}
+}
+
+func getLoginData() (string, string, error) {
+	f, err := os.ReadFile(filePath)
+	if err != nil {
+		return "", "", errors.New("Unable to get login data")
+	}
+
+	var loginData LoginData
+
+	if err := yaml.Unmarshal(f, &loginData); err != nil {
+		return "", "", err
+	}
+
+	return loginData.Username, loginData.Password, nil
+
+}
 
 func main() {
-
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
-	
-	greetUsers()
-	validateInput(firstName,lastName,email,userTickets)
-	wg.Wait()
-	}
-func greetUsers()  {
-		fmt.Printf("Welcome to %v booking application\n", conferenceName)
-		fmt.Printf("We have a total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
-		fmt.Println("Get your tickets here to attend")
+	// Get the login url
+	loginURL, err := extractLoginUrl()
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
 
-func sendTickets(userTickets uint, firstName string, lastName string, email string)  {
-	time.Sleep(10 * time.Second)
-	var ticket = fmt.Sprintf("%v tickets for %v %v", userTickets, firstName, lastName)
-	fmt.Println("-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-")
-	fmt.Printf("Sending Ticket:\n %v\n to email address %v\n",  ticket, email)
-	fmt.Println("-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-")
-	wg.Done()
-}
-func validateInput(fName string, lName string, email string, uTickets uint) {
+	// Get the login credentials from storage
+	username, password, err := getLoginData()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-	fmt.Println("Enter your first name:")
-	fmt.Scanln(&fName)
-
-
-	fmt.Println("Enter your last name:")
-	fmt.Scanln(&lName)
-
-	fmt.Println("Enter your email:")
-	fmt.Scanln(&email)
-
-	fmt.Println("Enter the number of tickets you want:")
-	fmt.Scanln(&uTickets)
-
-	isValidName := len(fName)>=2 && len(lName)>=2
-	isValidEmail := strings.Contains(email,"@")
-	isValidTicketNumber := uTickets>0 && uTickets <= remainingTickets
-
-	if isValidName && isValidEmail && isValidTicketNumber{
-		remainingTickets = remainingTickets - uTickets
-
-		var userData = userData{
-			firstName: fName,
-			lastName: lName,
-			email: email,
-			numTickets: uTickets,
-		}
-
-		bookings = append(bookings, userData)
-		fmt.Printf("List of bookings is %v\n",bookings)
-	
-		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", fName, lName, uTickets, email)
-		fmt.Printf("%v tickets are remaining for the %v\n", remainingTickets, conferenceName)
-
-		wg.Add(1)
-		go sendTickets(uTickets, fName,lName,email)
-	
-		fNames := []string{}
-		for _, booking := range bookings {
-				fNames = append(fNames, booking.firstName)
-			}
-			fmt.Printf("The first names of all our bookings are: %v\n",fNames)
-
-		
-		if remainingTickets == 0 {
-			fmt.Printf("%v is sold out. Come back next year\n", conferenceName)
-		}
+	// Send the login request
+	if err := login(loginURL, username, password); err != nil {
+		fmt.Println(err)
 	} else {
-		if !isValidName {
-			fmt.Println("Your firstname or lastname entered is too short")
-		}
-		if !isValidEmail {
-			fmt.Println("You entered an invalid e-mail address")
-		}
-		if !isValidTicketNumber {
-			fmt.Println("You entered an invalid ticket number")
-			fmt.Println("")
-		}
+		fmt.Println("Login successful")
 	}
 }
